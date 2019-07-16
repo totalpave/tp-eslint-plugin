9891a72fc6b4f75c43df007c2431275803fe0153
Norman Breau <norman.breau@totalpave.com>
2019-07-16 18:28:44 +0000

    disabled explicit-member-accessibility as it appears broken...

--- dist/configs/recommended.js
+++ dist/configs/recommended.js
@@ -47,7 +47,8 @@ exports.default = {
                 "@typescript-eslint/no-inferrable-types": "off",
                 "@typescript-eslint/no-angle-bracket-type-assertion": "off",
                 "@typescript-eslint/no-explicit-any": "off",
-                "@typescript-eslint/array-type": ['error', 'generic']
+                "@typescript-eslint/array-type": ['error', 'generic'],
+                "@typescript-eslint/explicit-member-accessibility": "off"
             }
         }
     ]
