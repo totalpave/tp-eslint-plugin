9929b2db74c3921910c3e70246ce823349aacf25
Norman Breau <norman@normanbreau.com>
2020-04-04 18:56:16 -0300

    react typescript eslint config

--- src/configs/recommendedReactTS.ts
+++ src/configs/recommendedReactTS.ts
@@ -2,7 +2,7 @@
 export default {
     overrides: [
         {
-            files: ['*.ts', '*.js', '*.jsx'],
+            files: ['*.tsx'],
             rules: {
                 "eol-last": ["error", "always"],
                 "array-bracket-spacing": ["error", "always"],
@@ -87,7 +87,7 @@ export default {
                 "@typescript-eslint/consistent-type-assertions": [
                     'error',
                     {
-                        assertionStyle: 'angle-bracket'
+                        assertionStyle: 'as'
                     }
                 ],
                 "@typescript-eslint/no-var-requires": "off",
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@
 import recommendedTS from './configs/recommended-ts';
 import recommended from './configs/recommended';
 import noDate from './rules/no-date';
+import recommendedReactTS from './configs/recommendedReactTS';
 // import * as unit from './unit';
 
 export = {
-	rules: {
-		'no-date': noDate
-	},
+    rules: {
+        'no-date': noDate
+    },
     configs: {
         recommended,
-        recommendedTS
+        recommendedTS,
+        recommendedReactTS
     }
 };
